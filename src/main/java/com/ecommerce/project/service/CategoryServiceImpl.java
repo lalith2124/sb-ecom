6ea9be23751f1d799bba5a7f924b2d5913e8f929
lalith2124 <lalithraju2102@gmail.com>
2026-02-16 01:20:18 +0530

    feat: implement Bean Validation and global exception handling
    - Added field-level validations using Jakarta Validation
    - Implemented centralized exception handling with @RestControllerAdvice
    - Added custom exception for handling empty category scenarios
    - Returned structured JSON error responses

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,15 +1,12 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -19,21 +16,27 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            throw new APIException("No category created till now.");
+        }
+        return categories;
     }
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null) {
+            throw new APIException(("Category with name " + category.getCategoryName() + " already exists !!"));
+        }
         categoryRepository.save(category);
     }
-
 
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() ->
-                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-
+                        new ResourceNotFoundException("Category","categoryId", categoryId));
         categoryRepository.delete(category);
         return "Category with categoryId: " + categoryId + " deleted successfully !!";
     }
@@ -43,8 +46,8 @@ public class CategoryServiceImpl implements CategoryService{
 
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow(() ->
-                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-        savedCategory.setCategoryName(category.getCategoryName());
+                        new ResourceNotFoundException("Category","categoryId", categoryId));
+        category.setCategoryId(categoryId);
         savedCategory = categoryRepository.save(savedCategory);
         return savedCategory;
     }
